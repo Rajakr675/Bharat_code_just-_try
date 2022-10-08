d486a68cc7b10c019be5615dba52cd0d4939194c
Raja Kumar Shah <97901981+Rajakr675@users.noreply.github.com>
2022-10-08 18:57:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const PORT = process.env.PORT || 5000;
 
 
 app.get('/',(req,res) => {
-    res.send('<h1>loudiya raja</h1>')
+    res.send('<h1>HOME</h1>')
 });
 
 app.get('/products',(req,res) => {
-    res.send('<h1>gandu raja</h1>')
+    res.send('<h1>YAHA PRODUCTS HAI</h1>')
 });
 
 app.get('/users',(req,res) => {
-    res.send('<h1>chutiya raja</h1>')
+    res.send('<h1>Yaha users nahi hai bhak saale</h1>')
 });
 
 app.listen(PORT,console.log(`server  started on port ${PORT}`));
